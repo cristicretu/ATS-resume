368fc7fafd5d6455b17bd7c5d22ccd04d06d12cd
Cristian Cretu <crisemcr@gmail.com>
2023-01-23 11:43:36 +0200

    refactor: change to multi-lang opposed to eng

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pypdf import PdfReader
 import spacy
 
 # Load the SpaCy model
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("xx_ent_wiki_sm")
 
 if __name__ == "__main__":
     res = sys.argv[1]
